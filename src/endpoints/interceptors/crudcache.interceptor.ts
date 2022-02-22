d63da799fbf886bf758e8c233cc0629e06736e30
Josh Gretz <jgretz@truefit.io>
2022-02-22 15:42:56 -0500

    finish out caching interceptor

--- src/endpoints/interceptors/crudcache.interceptor.ts
+++ src/endpoints/interceptors/crudcache.interceptor.ts
@@ -6,29 +6,48 @@ import {
   Inject,
   CACHE_MANAGER,
 } from '@nestjs/common';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Cache} from 'cache-manager';
 import {tap} from 'rxjs/operators';
+
+const CACHE_TTL = JSON.parse(process.env.CACHE_TTL);
+
+const valueFromCache = async (url: string, cacheManager: Cache) => {
+  return await cacheManager.get(url);
+};
+
+const cacheValue = (url: string, cacheManager: Cache, value: unknown) => {
+  const [request] = url.split('?');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_s, controller, resource, doc] = request.split('/');
+
+  if (controller !== 'crud') {
+    return;
+  }
+
+  const ttl = CACHE_TTL[`${resource}_${doc}`] || CACHE_TTL[`${resource}`];
+  if (!ttl) {
+    return;
+  }
+
+  cacheManager.set(url, value, {ttl});
+};
 
 @Injectable()
 export class CrudCacheInterceptor implements NestInterceptor {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.getArgByIndex(0) as Request;
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest<Request>();
     if (request.method !== 'GET') {
       return next.handle();
     }
 
-    console.log(request.url);
+    const cachedValue = await valueFromCache(request.url, this.cacheManager);
+    if (cachedValue !== undefined) {
+      return of(cachedValue);
+    }
 
-    return next.handle();
-
-    // const now = Date.now();
-    // return next
-    //   .handle()
-    //   .pipe(
-    //     tap(() => console.log(`After... ${Date.now() - now}ms`)),
-    //   );
+    return next.handle().pipe(tap((value) => cacheValue(request.url, this.cacheManager, value)));
   }
 }
